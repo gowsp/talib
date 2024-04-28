1a6a418138f0c7214b0fb98bf7881a5de360da66
hunter <wuzk@hst.com>
2024-04-28 21:36:53 +0800

    realpace map ro array

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/gowsp/talib
 
 go 1.16
 
-require github.com/shopspring/decimal v1.3.1
+require github.com/shopspring/decimal v1.4.0
--- time_series.go
+++ time_series.go
@@ -2,21 +2,18 @@ package talib
 
 import (
 	"sync"
-	"sync/atomic"
 	"time"
 
 	"github.com/shopspring/decimal"
 )
 
 type TimeSeries struct {
-	data       map[uint64]Bar
+	data       []Bar
 	period     uint64
-	latest     uint64
 	capacity   uint64
 	threshold  uint64
 	indicators sync.Map
 	mu         sync.RWMutex
-	isCleaning uint32
 }
 
 func NewSeries(period time.Duration) *TimeSeries {
@@ -29,7 +26,7 @@ func NewLimitSeries(period time.Duration, capacity, threshold uint64) *TimeSerie
 	return &TimeSeries{
 		capacity:  capacity,
 		threshold: threshold,
-		data:      map[uint64]Bar{},
+		data:      make([]Bar, 0, capacity),
 		period:    uint64(period.Seconds()),
 	}
 }
@@ -43,51 +40,16 @@ func (series *TimeSeries) LoadOrStore(id string, supplier func() Indicator) Indi
 }
 func (series *TimeSeries) Add(bar Bar) {
 	series.add(bar)
-	series.reduce()
 }
 func (series *TimeSeries) add(bar Bar) {
 	if uint64(bar.Period().Seconds()) != series.period {
 		return
 	}
-	valueTime := uint64(bar.BeginTime().Unix())
-	if valueTime%series.period != 0 {
-		return
-	}
-
 	series.mu.Lock()
 	defer series.mu.Unlock()
-	series.data[valueTime] = bar
-	if series.latest >= valueTime {
-		return
-	}
-	series.latest = valueTime
+	series.data = append(series.data, bar)
 }
-func (series *TimeSeries) reduce() {
-	series.mu.RLock()
-	size := uint64(len(series.data))
-	latest := series.latest
-	series.mu.RUnlock()
-	if size <= series.threshold || atomic.LoadUint32(&series.isCleaning) > 0 {
-		return
-	}
-	go series.clean(size, latest)
-}
-func (series *TimeSeries) clean(size, latest uint64) {
-	atomic.StoreUint32(&series.isCleaning, 1)
-	expired := make([]uint64, 0, size-series.capacity)
-	for i := size - 1; i >= series.capacity; i-- {
-		cursor := latest - series.period*i
-		series.mu.Lock()
-		delete(series.data, cursor)
-		series.mu.Unlock()
-		expired = append(expired, cursor)
-	}
-	series.indicators.Range(func(key, value interface{}) bool {
-		value.(Indicator).Delete(expired)
-		return true
-	})
-	atomic.StoreUint32(&series.isCleaning, 0)
-}
+
 func (series *TimeSeries) Size() uint64 {
 	series.mu.RLock()
 	defer series.mu.RUnlock()
@@ -96,13 +58,13 @@ func (series *TimeSeries) Size() uint64 {
 func (series *TimeSeries) Offset(i uint64) Bar {
 	series.mu.RLock()
 	defer series.mu.RUnlock()
-	cursor := series.latest - series.period*i
+	cursor := series.Cursor(i)
 	return series.data[cursor]
 }
 func (series *TimeSeries) Cursor(i uint64) uint64 {
 	series.mu.RLock()
 	defer series.mu.RUnlock()
-	return series.latest - series.period*i
+	return series.Size() - i - 1
 }
 
 func (series *TimeSeries) NewIndicator(id string, method func(Bar) decimal.Decimal) Indicator {
